bc5d813fd689bfb0167856388994846db84f6e9b
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-25 15:55:23 +0100

    added material, basic structure and styles

---
+++ counter/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CountComponent } from './count/count.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CountComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
