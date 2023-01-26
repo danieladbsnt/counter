fb9e6250ef1a799633756455725d3f186a54884e
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-26 13:40:46 +0100

    added start, pause and reset functions

--- counter/src/app/count/count.component.ts
+++ counter/src/app/count/count.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-count',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./count.component.css']
 })
 export class CountComponent implements OnInit {
-
+source = timer (1000,1000);
+subscription: any;
+isStart!:boolean;
+value = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+start() {
+  console.log('start');
+  this.isStart == true;
+  this.subscription = this.source.subscribe(val => val = this.value++)
+  
 }
+
+pause() {
+  console.log('pause');
+  this.isStart == false;
+  this.subscription.unsubscribe()
+}
+
+reset(){
+  console.log('reset');
+  this.value = 0;
+}
+
+countUp() {
+  this.value++
+}
+
+countDown() {
+  this.value--
+}
+}
+
+/*
+https://www.learnrxjs.io/learn-rxjs/operators/creation/timer
+https://www.learnrxjs.io/learn-rxjs/operators/transformation/map
+
+ */
